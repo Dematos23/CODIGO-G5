f6fd623c0c48dd8a29f292399ac89147f045df61
Diego Matos <dematos23@gmail.com>
2021-11-04 00:48:59 -0500

    semana 8 completo

--- Semana8/Dia2/04-fetch.js
+++ Semana8/Dia2/04-fetch.js
@@ -3,5 +3,29 @@ fetch("https://reqres.in/api/users?page=2")
     console.log(respuesta);
     return respuesta.json()
 }).then((gatos)=>{
-    console.log(gatos);
+    console.table(gatos.data);
 });
+
+let nuevoUsuario = {
+    name:"Pepe Lucho",
+    job:"BOSS"
+}
+
+let cabecera ={
+    method:"POST",
+    headers:{
+        'Content-Type':'application/json'
+    },
+    body:JSON.stringify(nuevoUsuario)
+};
+
+fetch("https://reqres.in/api/users", cabecera)
+.then((respuesta) => {
+    return respuesta.json();
+})
+.then((usuarioCreado) => {
+    console.table(usuarioCreado);
+})
+.catch((error)=>{
+    console.log(error);
+})
